e5439dadf630819e26f880bae5eec02deef57c69
Radek Brettschneider <brettsr@gmail.com>
2023-05-28 21:39:30 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+
+using Microsoft.Win32;
+using System.Text;
+using Tesseract;
+using Windows.UI.Xaml.Media;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace Griddler_Solver
+{
+  public partial class MainWindow : Window
+  {
+    private Solver _Solver
+    { get; set; } = new Solver();
+
+    private BitmapImage? InputImage
+    { get; set; }
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Draw()
+    {
+      canvas.Children.Clear();
+      _Solver.Draw(canvas);
+    }
+
+    private void OnSolve_Click(object sender, RoutedEventArgs e)
+    {
+      _Solver = new Solver();
+
+      Int32 black = 1;
+
+      // rows
+      ListSingleDefinition list = new ListSingleDefinition();
+      list.AddDefinition(black, 3);
+      _Solver.AddSingleDefinitionRow(list);
+
+      list = new ListSingleDefinition();
+      list.AddDefinition(black, 1);
+      list.AddDefinition(black, 1);
+      _Solver.AddSingleDefinitionRow(list);
+
+      list = new ListSingleDefinition();
+      list.AddDefinition(black, 2);
+      list.AddDefinition(black, 2);
+      _Solver.AddSingleDefinitionRow(list);
+
+      list = new ListSingleDefinition();
+      _Solver.AddSingleDefinitionRow(list);
+
+      list = new ListSingleDefinition();
+      list.AddDefinition(black, 5);
+      _Solver.AddSingleDefinitionRow(list);
+
+      // columns
+      list = new ListSingleDefinition();
+      list.AddDefinition(black, 1);
+      list.AddDefinition(black, 1);
+      _Solver.AddSingleDefinitionColumn(list);
+
+      list = new ListSingleDefinition();
+      list.AddDefinition(black, 3);
+      list.AddDefinition(black, 1);
+      _Solver.AddSingleDefinitionColumn(list);
+
+      list = new ListSingleDefinition();
+      list.AddDefinition(black, 1);
+      list.AddDefinition(black, 1);
+      _Solver.AddSingleDefinitionColumn(list);
+
+      list = new ListSingleDefinition();
+      list.AddDefinition(black, 3);
+      list.AddDefinition(black, 1);
+      _Solver.AddSingleDefinitionColumn(list);
+
+      list = new ListSingleDefinition();
+      list.AddDefinition(black, 1);
+      list.AddDefinition(black, 1);
+      _Solver.AddSingleDefinitionColumn(list);
+
+      _Solver.Solve();
+      Draw();
+    }
+
+    private void OnLoadInput_Click(object sender, RoutedEventArgs e)
+    {
+      OpenFileDialog openFileDialog = new OpenFileDialog();
+      if (openFileDialog.ShowDialog() == true)
+      {
+        System.Windows.Controls.Image imageControl = new System.Windows.Controls.Image();
+        imageControl.Stretch = System.Windows.Media.Stretch.None;
+
+        var image = new BitmapImage(new Uri(openFileDialog.FileName));
+        toCCITT(openFileDialog.FileName);
+
+        imageControl.Source = InputImage = image;
+
+        canvas.Children.Clear();
+        canvas.Children.Add(imageControl);
+      }
+    }
+
+    public void toCCITT(string tifURL)
+    {
+      byte[] imgBits = File.ReadAllBytes(tifURL);
+
+      using (MemoryStream ms = new MemoryStream(imgBits))
+      {
+        using (Image i = Image.FromStream(ms))
+        {
+          EncoderParameters parms = new EncoderParameters(1);
+          ImageCodecInfo codec = ImageCodecInfo.GetImageDecoders().FirstOrDefault(decoder => decoder.FormatID == System.Drawing.Imaging.ImageFormat.Tiff.Guid);
+
+          parms.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Compression, (long)EncoderValue.CompressionCCITT4);
+
+          i.Save(@"d:\Downloads\result.tif", codec, parms);
+        }
+      }
+    }
+
+    public void InvertColor(Image image)
+    {
+      Bitmap pic = new Bitmap(image);
+      for (int y = 0; (y <= (pic.Height - 1)); y++)
+      {
+        for (int x = 0; (x <= (pic.Width - 1)); x++)
+        {
+          System.Drawing.Color inv = pic.GetPixel(x, y);
+          inv = System.Drawing.Color.FromArgb(255, (255 - inv.R), (255 - inv.G), (255 - inv.B));
+          pic.SetPixel(x, y, inv);
+        }
+      }
+    }
+    public void ToGrayScale(Image image)
+    {
+      Bitmap bmp = new Bitmap(image);
+
+      int rgb;
+      System.Drawing.Color c;
+
+      for (int y = 0; y < bmp.Height; y++)
+        for (int x = 0; x < bmp.Width; x++)
+        {
+          c = bmp.GetPixel(x, y);
+          rgb = (int)Math.Round(.299 * c.R + .587 * c.G + .114 * c.B);
+          bmp.SetPixel(x, y, System.Drawing.Color.FromArgb(rgb, rgb, rgb));
+        }
+    }
+
+    // https://www.griddlers.net/cs_CZ/nonogram/-/g/183521
+    private async void OnCanvas_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+      if (InputImage == null)
+      {
+        return;
+      }
+
+      String currentDirectory = Directory.GetCurrentDirectory();
+      String[] arrFiles = Directory.GetFiles(currentDirectory, "*.bmp");
+      foreach (String file in arrFiles)
+      {
+        File.Delete(file);
+      }
+
+      Int32 cellSize = 15;
+
+      System.Windows.Point p = Mouse.GetPosition(canvas);
+
+      Int32 left = (Int32)p.X;
+      Int32 top = (Int32)p.Y;
+
+      Int32 mainLeft = left / cellSize;
+      for (Int32 counter = 1; counter <= mainLeft; counter++)
+      {
+        Int32 leftNew = left - (counter * cellSize);
+        
+        CroppedBitmap croppedBitmap = new CroppedBitmap(InputImage, new Int32Rect(leftNew, top, cellSize, cellSize));
+        //ToGrayScale(croppedBitmap);
+
+        BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+        encoder.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(croppedBitmap));
+
+        String fileTitle = $"cell_{leftNew:000}.{top:000}.bmp";
+        String fileName = Path.Combine(currentDirectory, fileTitle);
+        
+        FileStream filestream = new FileStream(fileName, FileMode.Create);
+        encoder.Save(filestream);
+
+        filestream.Close();
+
+        var ocrtext = string.Empty;
+        using (var engine2 = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+        {
+          using (var img = Pix.LoadFromFile(fileName))
+          {
+            for (Int32 step = 0; step < 2; step++)
+            {
+              using (var page = engine2.Process(img))
+              {
+                ocrtext = page.GetText();
+                Console.Write("aaa");
+                Console.WriteLine(ocrtext);
+                if (ocrtext != String.Empty)
+                {
+                  int g = 0;
+                }
+              }
+            }
+          }
+        }
+
+        var engine = OcrEngine.TryCreateFromUserProfileLanguages();
+        //var engine = OcrEngine.TryCreateFromLanguage(new Windows.Globalization.Language("en-US"));
+        var file = await Windows.Storage.StorageFile.GetFileFromPathAsync(fileName);
+        var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+        var decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(stream);
+        var softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+        var ocrResult = await engine.RecognizeAsync(softwareBitmap);
+        Console.WriteLine(ocrResult.Text);
+        if (ocrResult.Text != String.Empty)
+        {
+          int g = 0;
+        }
+
+
+        //Windows.Graphics.Imaging.BitmapDecoder decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(new MemoryStream(memoryStream.ToArray()));
+        //SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+        //var ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+        //var ocrResult = await ocrEngine.RecognizeAsync(softwareBitmap);
+        //foreach (var line in ocrResult.Lines)
+        {
+          //Console.WriteLine(line.Text);
+        }
+
+      }
+
+
+      /*System.Windows.Controls.Image imageControl = new System.Windows.Controls.Image();
+      imageControl.Stretch = Stretch.None;
+      imageControl.Source = cropped_bitmap;
+      canvas.Children.Add(imageControl);
+
+      JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+      MemoryStream memoryStream = new MemoryStream();
+      BitmapImage bImg = new BitmapImage();
+
+      encoder.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(cropped_bitmap));
+      encoder.Save(memoryStream);
+
+      memoryStream.Position = 0;
+      bImg.BeginInit();
+      bImg.StreamSource = new MemoryStream(memoryStream.ToArray());
+      bImg.EndInit();
+
+      //memoryStream.Close();
+
+      {
+        Windows.Graphics.Imaging.BitmapDecoder decoder = await Windows.Graphics.Imaging.BitmapDecoder.CreateAsync(new MemoryStream(memoryStream.ToArray()));
+        SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+        var ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+        var ocrResult = await ocrEngine.RecognizeAsync(softwareBitmap);
+        foreach (var line in ocrResult.Lines)
+        {
+          Console.WriteLine(line.Text);
+        }
+      }*/
+    }
+
+    private void OnButtonGo_Click(object sender, RoutedEventArgs e)
+    {
+      // https://www.griddlers.net/en_US/nonogram/-/g/t1679057429974/i01?p_p_lifecycle=2&p_p_resource_id=griddlerPuzzle&p_p_cacheability=cacheLevelPage&_gpuzzles_WAR_puzzles_id={id}&_gpuzzles_WAR_puzzles_lite=false
+    }
+  }
+}
---
+++ Solver.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Griddler_Solver
+{
+  internal class Definition
+  {
+    public static Int32 ColorBackground = 0;
+    public static Definition DefinitionBackground = new Definition() { Color = ColorBackground, Count = 1 };
+
+    public Int32 Color
+    { get; set; }
+
+    public Int32 Count
+    { get; set; }
+
+    public Boolean IsBackground
+    {
+      get
+      {
+        return (Color == ColorBackground);
+      }
+    }
+
+    public override String ToString()
+    {
+      return $"[{Color}:{Count}]";
+    }
+  }
+  internal class ListSingleDefinition
+  {
+    public List<Definition> Data
+    { get; private set; } = new List<Definition>();
+
+    public Boolean Solved
+    { get; set; }
+
+    public void AddDefinition(Int32 color, Int32 count)
+    {
+      Debug.Assert(color != Definition.ColorBackground);
+
+      if (Data.Count > 0)
+      {
+        if (Data[Data.Count - 1].Color == color)
+        {
+          Data.Add(Definition.DefinitionBackground);
+        }
+      }
+      Data.Add(new Definition { Color = color,Count = count });
+    }
+
+    public override String ToString()
+    {
+      return $"[{Data.Count}:{Solved}]";
+    }
+  }
+
+  internal class ListDefinition : List<ListSingleDefinition>
+  {
+  }
+
+  internal class Solver
+  {
+    private const Int32 cellSize = 50;
+    private readonly SolidColorBrush BrushLime = new SolidColorBrush(Colors.Lime);
+
+    private Int32 CountRows
+    {
+      get
+      {
+        return DefinitionListRows.Count;
+      }
+    }
+    private Int32 CountColumns
+    {
+      get
+      {
+        return DefinitionListColumns.Count;
+      }
+    }
+
+    private ListDefinition DefinitionListRows = new ListDefinition();
+    private ListDefinition DefinitionListColumns = new ListDefinition();
+
+    private Boolean ChangeMade
+    { get; set; }
+    private Definition[,] _Board = new Definition[0,0];
+
+    public void AddSingleDefinitionRow(ListSingleDefinition list)
+    {
+      DefinitionListRows.Add(list);
+    }
+    public void AddSingleDefinitionColumn(ListSingleDefinition list)
+    {
+      DefinitionListColumns.Add(list);
+    }
+
+    private Rectangle CreateRectangle(Int32 left, Int32 top, SolidColorBrush brush)
+    {
+      Rectangle rect = new Rectangle();
+
+      rect.Stroke = brush;
+      rect.Fill = brush;
+
+      rect.Width = cellSize;
+      rect.Height = cellSize;
+      Canvas.SetLeft(rect, left);
+      Canvas.SetTop(rect, top);
+
+      return rect;
+    }
+
+    public void Draw(Canvas canvas)
+    {
+      for (Int32 row = 0; row < CountRows; row++)
+      {
+        ListSingleDefinition list = DefinitionListRows[row];
+        if (list.Solved)
+        {
+          canvas.Children.Add(CreateRectangle(0, cellSize + row * cellSize, BrushLime));
+        }
+
+        for (Int32 column = 0; column < CountColumns; column++)
+        {
+          if (row == 0)
+          {
+            list = DefinitionListColumns[column];
+            if (list.Solved)
+            {
+              canvas.Children.Add(CreateRectangle(cellSize + column * cellSize, 0, BrushLime));
+            }
+          }
+
+          SolidColorBrush brush;
+          if (_Board[row, column] == null)
+          {
+            brush = new SolidColorBrush(Colors.LightGray);
+          }
+          else if (_Board[row, column].Color == Definition.ColorBackground)
+          {
+            brush = new SolidColorBrush(Colors.White);
+          }
+          else
+          {
+            brush = new SolidColorBrush(Colors.Black);
+          }
+
+          canvas.Children.Add(CreateRectangle(cellSize + column * cellSize, cellSize + row * cellSize, brush));
+        }
+      }
+    }
+
+    private Definition GetCell(Boolean isRow, Int32 indexFirst, Int32 indexSecond)
+    {
+      if (isRow)
+      {
+        return _Board[indexFirst, indexSecond];
+      }
+      else
+      {
+        return _Board[indexSecond, indexFirst];
+      }
+    }
+    private void SetCell(Boolean isRow, Int32 indexFirst, Int32 indexSecond, Definition definition)
+    {
+      if (isRow)
+      {
+        _Board[indexFirst, indexSecond] = definition;
+      }
+      else
+      {
+        _Board[indexSecond, indexFirst] = definition;
+      }
+      ChangeMade = true;
+    }
+
+    private void CheckDefinitionList(ListDefinition listDefinition, Boolean isRows)
+    {
+      Int32 countFirst = isRows ? CountRows : CountColumns;
+      Int32 countSecond = isRows ? CountColumns : CountRows;
+
+      // check out if SingleDefinitionList fits whole row/column
+      for (Int32 indexFirst = 0; indexFirst < countFirst; indexFirst++)
+      {
+        ListSingleDefinition list = listDefinition[indexFirst];
+        if (list.Solved)
+        {
+          continue;
+        }
+
+        // empty row/column
+        if (list.Data.Count == 0)
+        {
+          for (Int32 indexSecond = 0; indexSecond < countSecond; indexSecond++)
+          {
+            SetCell(isRows, indexFirst, indexSecond, Definition.DefinitionBackground);
+          }
+
+          list.Solved = true;
+          continue;
+        }
+
+        Int32 currentSecond = 0;
+        Boolean doNotFit = false;
+
+        foreach (Definition definition in list.Data)
+        {
+          if (doNotFit)
+          {
+            break;
+          }
+
+          for (Int32 indexSecond = 0; indexSecond < definition.Count; indexSecond++)
+          {
+            Definition cell = GetCell(isRows, indexFirst, currentSecond);
+            if (cell != null && cell.Color != definition.Color)
+            {
+              doNotFit = true;
+              break;
+            }
+
+            currentSecond++;
+          }
+        }
+
+        if (currentSecond == countSecond && doNotFit == false)
+        {
+          currentSecond = 0;
+          foreach (Definition definition in list.Data)
+          {
+            for (Int32 indexSecond = 0; indexSecond < definition.Count; indexSecond++)
+            {
+              SetCell(isRows, indexFirst, currentSecond, definition);
+              currentSecond++;
+            }
+          }
+
+          list.Solved = true;
+        }
+      }
+
+      // check if row/column is solved and fill backgrounds instead null
+      for (Int32 indexFirst = 0; indexFirst < countFirst; indexFirst++)
+      {
+        ListSingleDefinition list = listDefinition[indexFirst];
+        if (list.Solved)
+        {
+          continue;
+        }
+
+        Int32 currentSecond = 0;
+        Boolean solved = true;
+
+        foreach (Definition definition in list.Data)
+        {
+          if (solved == false)
+          {
+            break;
+          }
+
+          if (definition.IsBackground)
+          {
+            continue;
+          }
+
+          for (Int32 indexSecond = 0; indexSecond < definition.Count; indexSecond++)
+          {
+            if (solved == false || currentSecond >= countSecond)
+            {
+              solved = false;
+              break;
+            }
+
+            Definition cell = GetCell(isRows, indexFirst, currentSecond);
+
+            // find first not null cell
+            while (cell == null || cell.IsBackground)
+            {
+              currentSecond++;
+              if (currentSecond >= countSecond)
+              {
+                solved = false;
+                break;
+              }
+
+              cell = GetCell(isRows, indexFirst, currentSecond);
+              continue;
+            }
+
+            if (solved == false || cell == null || cell.Color != definition.Color)
+            {
+              solved = false;
+              continue;
+            }
+
+            currentSecond++;
+          }
+        }
+
+        if (solved)
+        {
+          for (Int32 indexSecond = 0; indexSecond < countSecond; indexSecond++)
+          {
+            Definition cell = GetCell(isRows, indexFirst, indexSecond);
+            if (cell == null)
+            {
+              SetCell(isRows, indexFirst, indexSecond, Definition.DefinitionBackground);
+            }
+          }
+          
+          list.Solved = true;
+        }
+      }
+    }
+
+    public Boolean Solve()
+    {
+      _Board = new Definition[CountRows, CountColumns];
+
+      ChangeMade = true;
+      while (ChangeMade)
+      {
+        ChangeMade = false;
+
+        CheckDefinitionList(DefinitionListRows, true);
+        CheckDefinitionList(DefinitionListColumns, false);
+      }
+
+      return true;
+    }
+  }
+}
